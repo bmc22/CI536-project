b6514bc722133881eef407b4c6f39a78ffd14dfb
bmc22 <90981587+bmc22@users.noreply.github.com>
2022-05-10 16:01:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+
+window.addEventListener("load", function(){
+	
+	//********************** the books page *******************
+	document.querySelector("#books").addEventListener("click", function(){ 
+		window.open("books.html", "_self")
+	})
+	
+	
+	//********************** the electronics page *******************
+	document.querySelector("#electronics").addEventListener("click", function(){ 
+		window.open("electronics.html", "_self")
+	})
+	
+	
+	//********************** the stationery page *******************
+	document.querySelector("#stationery").addEventListener("click", function(){ 
+		window.open("stationery.html", "_self")
+	})
+	
+	
+	//********************** the furniture page *******************
+	document.querySelector("#furniture").addEventListener("click", function(){ 
+		window.open("furniture.html", "_self")
+	})
+	
+	
+	//********************** the food page *******************
+	document.querySelector("#food").addEventListener("click", function(){ 
+		window.open("food.html", "_self")
+	})
+	
+	
+	//********************** the gardening page *******************
+	document.querySelector("#gardening").addEventListener("click", function(){ 
+		window.open("gardening.html", "_self")
+	})
+	
+	
+
+ }
+);
---
+++ products.js
@@ -0,0 +1,42 @@
+
+window.addEventListener("load", function() {
+
+	document.querySelector("#filter").addEventListener("click", function () {
+		document.querySelector("#filters").style.width = "250px";
+	})
+
+	document.querySelector(".closebtn").addEventListener("click", function () {
+		document.querySelector("#filters").style.width = "0";
+	})
+
+	document.querySelector("#apply-filters").addEventListener("click", function(){
+		document.querySelector("#filters").style.width = "0";
+	})
+	
+	
+	var stars= document.querySelectorAll(".fa-star");
+
+	for (let i =0; i < stars.length ; i++){
+		stars[i].addEventListener("click", function(){
+		    
+			for( var j = 0 ; j < i + 1 ; j++) {
+				stars[j].classList.add("checked");
+				stars[j].classList.remove("unchecked");
+			}
+			
+			for( var j = i + 1 ; j < stars.length ; j++) {
+				stars[j].classList.add("unchecked");
+				stars[j].classList.remove("checked");
+			}
+			
+			
+		    document.querySelector("#stars").setAttribute("value", i+1);
+		})
+	}
+	
+})
+
+
+window.addEventListener("scroll", function() {
+	document.querySelector("#filters").style.width = "0";
+})
